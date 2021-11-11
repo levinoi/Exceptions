ea768dc797a3bb6354d186a814d81f0b75ced2df
levinoi <levi.alya@gmail.com>
2021-11-11 23:19:02 +0600

    exceptions methods

---
+++ Exception.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace My_hometask_2
+{
+    public class MyhometaskException : System.Exception
+    {
+        public MyhometaskException(string message) : base(message) { }
+    }
+
+    class NonElectroDeviceException : MyhometaskException
+    {
+        public NonElectroDeviceException(string message) : base(message) { }
+    }
+
+    class NullPowerException : MyhometaskException
+    {
+        public NullPowerException(string message) : base(message) { }
+    }
+
+    class OutOfRangeException : MyhometaskException
+    {
+        public OutOfRangeException(string message) : base(message) { }
+    }
+    class OutOfCategoryException : MyhometaskException
+    {
+        public OutOfCategoryException(string message) : base(message) { }
+    }
+}
+
+
+
